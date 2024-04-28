3aed1cedf995f24269a6c803f363e2b813fd904f
anoobsuresh04 <anoob.suresh@perleybrook.com>
2024-04-28 21:40:39 +0530

    feat: Create Card component design

---
+++ components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <>
+      <div className=" w-[300px] h-[300px] bg-[#5A5A5A] rounded-2xl my-4 p-4  ">
+        <h2 className=" font-sulphurPoint text-[36px] ">01․ </h2>
+
+        <h3 className=" font-sulphurPoint mb-4 ">EXECUTIVE BRIEFING</h3>
+
+        <h4 className=" font-neueMachina">
+          Lorem ipsum dolor sit amet consec tetur lorem ipsum. Dolor sit amet
+          consectetur. Lorem ipsum dolor sit amet consectetur.
+        </h4>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -1,28 +1,36 @@
 import React from "react";
 import BrainImage from "../../../assets/images/image_brain.png";
 import SearchBar from "@/components/SearchBar/SearchBar";
+import Card from "@/components/Card/Card";
 
 const HeroSection = () => {
   return (
-    <div className="flex items-center h-screen gap-10 ">
-      <div className="w-[60%]">
-        <h2 className=" font-powerGrotesk text-[#666666] ">
-          Your Journey to Tomorrow Begins Here
-        </h2>
-        <h1 className=" font-sulphurPoint">
-          Explore the Frontiers of Artificial Intelligence
-        </h1>
-        <h4 className=" font-neueMachina text-[#666666] ">
-          Welcome to the epicenter of AI innovation. FutureTech AI News is your
-          passport to a world where machines think, learn, and reshape the
-          future. Join us on this visionary expedition into the heart of AI.
-        </h4>
+    <div className="flex h-screen items-center ">
+      <div className="flex-cols ">
+        <div className="flex items-center gap-10 ">
+          <div className="w-[60%]">
+            <h2 className=" font-powerGrotesk text-[#666666] ">
+              Your Journey to Tomorrow Begins Here
+            </h2>
+            <h1 className=" font-sulphurPoint">
+              Explore the Frontiers of Artificial Intelligence
+            </h1>
+            <h4 className=" font-neueMachina text-[#666666] ">
+              Welcome to the epicenter of AI innovation. FutureTech AI News is
+              your passport to a world where machines think, learn, and reshape
+              the future. Join us on this visionary expedition into the heart of
+              AI.
+            </h4>
 
-        <SearchBar />
-      </div>
+            <SearchBar />
+          </div>
 
-      <div className="w-[40%]">
-        <img src={BrainImage.src} alt="" />
+          <div className="w-[40%]">
+            <img src={BrainImage.src} alt="" />
+          </div>
+        </div>
+
+          <Card />
       </div>
     </div>
   );
