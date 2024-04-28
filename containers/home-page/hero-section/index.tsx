32e37f2d92d986816bf09109f9364ae5eeb49f57
anoobsuresh04 <anoob.suresh@perleybrook.com>
2024-04-28 20:19:44 +0530

    feat: Add the search bar and icons and clear search func

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -1,27 +1,29 @@
 import React from "react";
-import BrainImage from "../../../assets/images/image_brain.png"
+import BrainImage from "../../../assets/images/image_brain.png";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 const HeroSection = () => {
   return (
-    <div className="flex items-center h-screen  ">
-      <div className="w-[60%]" >
-        <h2 className=" font-powerGrotesk text-[30px] text-[#666666] ">
+    <div className="flex items-center h-screen gap-10 ">
+      <div className="w-[60%]">
+        <h2 className=" font-powerGrotesk text-[#666666] ">
           Your Journey to Tomorrow Begins Here
         </h2>
         <h1 className=" font-sulphurPoint">
           Explore the Frontiers of Artificial Intelligence
         </h1>
-        <h3 className=" font-neueMachina text-[18px] text-[#666666] ">
+        <h4 className=" font-neueMachina text-[#666666] ">
           Welcome to the epicenter of AI innovation. FutureTech AI News is your
           passport to a world where machines think, learn, and reshape the
           future. Join us on this visionary expedition into the heart of AI.
-        </h3>
+        </h4>
+
+        <SearchBar />
       </div>
 
-      <div className="w-[40%]" >
-       <img src={BrainImage.src} alt="" />
+      <div className="w-[40%]">
+        <img src={BrainImage.src} alt="" />
       </div>
-      
     </div>
   );
 };
