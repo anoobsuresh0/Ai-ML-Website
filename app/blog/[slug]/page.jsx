3f95c22fe320d5ac123cdd4192d5cdfc9d9a7bd8
anoobsuresh04 <anoob.suresh@perleybrook.com>
2024-06-15 00:46:19 +0530

    feat: Add reponsive to the prose blog content

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -16,13 +16,16 @@ const Page = async ({ params }) => {
   const { content } = await getPageContent(params.slug);
 
   return (
-    <section className="py-10 h-[100%] ">
-      <Link href="/blog" className="flex  items-center px-[300px]">
+    <section className="py-10 h-[100%]">
+      <Link
+        href="/blog"
+        className="flex  items-center lg:px-[300px] md:px-[300px] sm:px-4 mb-4 "
+      >
         <IoMdArrowRoundBack style={{ fontSize: 20, marginRight: 10 }} />
         <h4>Back</h4>
       </Link>
 
-      <div className="container px-[300px] text-white justify-center py-4 prose max-w-none ">
+      <div className="container mx-auto px-4 md:px-[300px] lg:px-[300px] text-white justify-center py-4 prose  max-w-none ">
         {content}
       </div>
     </section>
